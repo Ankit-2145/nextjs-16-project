0f67c6b943fb912409aaf5eb22c2d06acd58e221
Ankit Sharma <ankitsharma2615@gmail.com>
2025-11-05 13:00:54 +0530

    implement api-routes, server actions, caching

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 import connectDB from "@/lib/mongodb";
 import Event from "@/database/event.model";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
@@ -19,6 +20,35 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    const file = formData.get("image") as File;
+
+    if (!file) {
+      return NextResponse.json(
+        { message: "Image file is required" },
+        { status: 400 }
+      );
+    }
+
+    const arrayBuffer = await file.arrayBuffer(); // Convert to raw data
+    const buffer = Buffer.from(arrayBuffer); // Convert to Node.js buffer
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "image",
+            folder: "DevEvent",
+          },
+          (error, results) => {
+            if (error) return reject(error);
+            resolve(results);
+          }
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
     const createdEvent = await Event.create(event);
 
     return NextResponse.json(
@@ -36,6 +66,24 @@ export async function POST(req: NextRequest) {
         error: e instanceof Error ? e.message : "Unknown",
       },
       { status: 400 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      { message: "Events fetched successfully", events },
+      { status: 200 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Event fetching failed", error: e },
+      { status: 500 }
     );
   }
 }
